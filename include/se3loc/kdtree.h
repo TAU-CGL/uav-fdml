125c01ffdf810d0a6aa3fa298e66e0801a0647df
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2024-02-16 19:33:39 +0200

    Bugfix: KDTree is now correct and so is NN

--- include/se3loc/kdtree.h
+++ include/se3loc/kdtree.h
@@ -4,6 +4,9 @@
 #include <boost/range/algorithm/sort.hpp>
 #include <boost/algorithm/cxx11/any_of.hpp>
 
+#include <fmt/format.h>
+using fmt::format;
+
 #include "geometry.h"
 
 namespace se3loc {
@@ -32,12 +35,16 @@ namespace se3loc {
         }
 
         Point<dim, dtype> nearestNeighbor(Point<dim, dtype> q) {
-            // Point<dim, dtype> p = findCell(q, root);
-            // dtype w = sqrt((p - q).squaredNorm());
             Point<dim, dtype> p;
             dtype w = -1;
             nearestNeighborRecurse(q, root, p, w);
             return p;
+        }
+
+        std::string str() const {
+            std::string res;
+            strRecurse(root, res, 0);
+            return res;
         }
 
     private:
@@ -48,7 +55,7 @@ namespace se3loc {
         uint32_t _tmpSize;
 
         KDTreeNode<dim, dtype>* fitRecurse(int32_t left, int32_t right, int32_t depth) {
-            if (right < left) return nullptr;
+            if (right <= left) return nullptr;
             if (left >= _points.size()) return nullptr;
 
             uint8_t cutDim = (uint8_t)depth % dim;
@@ -57,73 +64,43 @@ namespace se3loc {
             int32_t middle = left + (right - left) / 2;
             dtype median = _points[middle][cutDim];
             KDTreeNode<dim, dtype>* node = &_nodes[_tmpSize++];
-            if (left == right) node->point = _points[middle];
+            node->point = _points[middle];
             node->cutDim = cutDim;
             node->cutValue = median;
 
-            node->left = fitRecurse(left, middle - 1, depth + 1);
+            node->left = fitRecurse(left, middle, depth + 1);
             node->right = fitRecurse(middle + 1, right, depth + 1);
 
             return node;
         }
 
-        Point<dim, dtype> findCell(Point<dim, dtype> q, KDTreeNode<dim, dtype>* node) {
-            if (q[node->cutDim] <= node->cutValue) {
-                if (!node->left) return node->point;
-                return findCell(q, node->left);
-            } else {
-                if (!node->right) return node->point;
-                return findCell(q, node->right);
-            }
+        void strRecurse(KDTreeNode<dim, dtype>* node, std::string& res, int32_t depth) const {
+            if (!node) return;
+            for (int32_t i = 0; i < depth; ++i) res += "\t";
+            res += format("cut@{} val={} {}\n", node->cutDim, node->cutValue, node->point.str());
+            strRecurse(node->left, res, depth + 1);
+            strRecurse(node->right, res, depth + 1);
         }
 
         void nearestNeighborRecurse(Point<dim, dtype> q, KDTreeNode<dim, dtype>* node, Point<dim, dtype>& p, dtype& w) {
-            if (!node->left) {
-                dtype w_ = std::sqrt((q - node->point).squaredNorm());
-                if (w < 0 ||w_ < w) {
-                    w = w_;
-                    p = node->point;
-                }
-                return;
-            }
-
-            // nearestNeighborRecurse(q, node->left, p, w);
-            // nearestNeighborRecurse(q, node->right, p, w);
-
-            if (w < 0) {
-                if (q[node->cutDim] <= node->cutValue) {
-                    nearestNeighborRecurse(q, node->left, p, w);
-                    if (q[node->cutDim] + w > node->cutValue) nearestNeighborRecurse(q, node->right, p, w);
-                } else {
-                    nearestNeighborRecurse(q, node->right, p, w);
-                    if (q[node->cutDim] - w <= node->cutValue) nearestNeighborRecurse(q, node->left, p, w);
-                }
-            } else {
-                if (q[node->cutDim] - w <= node->cutValue) nearestNeighborRecurse(q, node->left, p, w);
-                if (q[node->cutDim] + w > node->cutValue) nearestNeighborRecurse(q, node->right, p, w);
-            }
-
-        }
-
-        // void nearestNeighborRecurse(Point<dim, dtype> q, KDTreeNode<dim, dtype>* node, Point<dim, dtype>& p, dtype& w) {
-        //     if (!node) return;
+            if (!node) return;
             
-        //     dtype w_ = std::sqrt((q - node->point).squaredNorm());
-        //     if (w_ < w) {
-        //         w = w_;
-        //         p = node->point;
-        //     }
+            dtype w_ = std::sqrt((q - node->point).squaredNorm());
+            if (w < 0 || w_ < w) {
+                w = w_;
+                p = node->point;
+            }
 
-        //     bool searchLeft = q[node->cutDim] <= node->cutValue;
-        //     dtype diff = abs(q[node->cutDim] - node->cutValue); // / (dim * dim * 4);
-        //     if (searchLeft) {
-        //         nearestNeighborRecurse(q, node->left, p, w);
-        //         if (diff < w) nearestNeighborRecurse(q, node->right, p, w);
-        //     } else {
-        //         nearestNeighborRecurse(q, node->right, p, w);
-        //         if (diff < w) nearestNeighborRecurse(q, node->left, p, w);
-        //     }
-        // }
+            bool searchLeft = q[node->cutDim] <= node->cutValue;
+            dtype diff = abs(q[node->cutDim] - node->cutValue);
+            if (searchLeft) {
+                nearestNeighborRecurse(q, node->left, p, w);
+                if (diff < w) nearestNeighborRecurse(q, node->right, p, w);
+            } else {
+                nearestNeighborRecurse(q, node->right, p, w);
+                if (diff < w) nearestNeighborRecurse(q, node->left, p, w);
+            }
+        }
 
         // ------------------------------------------------------------------------
 
--- tests/src/test_kdtree.cpp
+++ tests/src/test_kdtree.cpp
@@ -6,8 +6,8 @@ using fmt::format, fmt::print;
 
 #include "se3loc/se3loc.h"
 
-constexpr uint8_t DIM = 2;
-constexpr int32_t NUM_POINTS = 16;
+constexpr uint8_t DIM = 3;
+constexpr int32_t NUM_POINTS = 4096;
 
 TEST(KDTreeTest, TestBuildTree) {
     se3loc::Random::seed(0);
@@ -23,22 +23,17 @@ TEST(KDTreeTest, TestNearestNeighbors) {
     for (int32_t i = 0; i < NUM_POINTS; ++i) points.push_back(se3loc::Point<DIM, double>::uniform());
     se3loc::KDTree<DIM, double> kdtree;
     kdtree.fit(points);
-    // for (auto point : kdtree._points) {
-    //     print("{} {}; ", point[0], point[1]);
-    // }
-    // print("\n");
-
+    
     boost::container::vector<se3loc::Point<DIM, double>> qs;
     for (int32_t i = 0; i < NUM_POINTS; ++i) qs.push_back(se3loc::Point<DIM, double>::uniform());
     int tmp = 0;
     for (auto q : qs) {
-        if (tmp++ != 7) continue; 
         // Get ground truth
         se3loc::Point<DIM, double> gt;
-        double minDist = 1e30;
+        double minDist = -1;
         for (auto p : points) {
             double dist = std::sqrt((q - p).squaredNorm());
-            if (dist < minDist) {
+            if (minDist < 0 || dist < minDist) {
                 gt = p;
                 minDist = dist;
             }
@@ -47,9 +42,7 @@ TEST(KDTreeTest, TestNearestNeighbors) {
         // Get nearest neighbor
         se3loc::Point<DIM, double> neighbor = kdtree.nearestNeighbor(q);
 
-        double err = abs(std::sqrt((q - gt).squaredNorm()) - std::sqrt((q - neighbor).squaredNorm()));
-        EXPECT_LT(err, 1e-4);
-
-
+        // Check that we get exactly the same point
+        for (int j = 0; j < DIM; j++) EXPECT_EQ(neighbor[j], gt[j]);
     }
 }

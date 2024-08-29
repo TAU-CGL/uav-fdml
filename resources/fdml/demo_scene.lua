f4c92030cff2c09ff77911f37a66157fbb3d92ba
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2024-08-29 17:11:12 +0300

    Added CaseStudy2

--- resources/fdml/demo_scene.lua
+++ resources/fdml/demo_scene.lua
@@ -67,7 +67,7 @@ Scene.StaticMeshes = {
     -- },
     {
         Name = "SM_room",
-        Path = "/fdml/scans/isprs/cs1.obj",
+        Path = "/fdml/scans/isprs/cs2.obj",
         KeepData = true
     },
     {
